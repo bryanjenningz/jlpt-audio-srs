9755644be2f9463e294e514c955e93567e83bc73
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-07 12:25:19 -0700

    Add fetch words

---
+++ src/words/fetch.ts
@@ -0,0 +1,17 @@
+import { type Word } from "~/words/types";
+
+export async function fetchWords(): Promise<Word[]> {
+  const response = await fetch("/jlpt5.txt");
+  const text = await response.text();
+  const lines = text.split("\n");
+  const words = lines.map((line): Word => {
+    const sections = line.split(";");
+    const japanese = sections[0];
+    const english = sections[sections.length - 1];
+    if (sections.length < 2 || sections.length > 3 || !japanese || !english) {
+      throw new Error(`Expected 2 or 3 non-empty sections for line: "${line}"`);
+    }
+    return { japanese, english };
+  });
+  return words;
+}
---
+++ src/words/types.ts
@@ -0,0 +1,4 @@
+export type Word = {
+  japanese: string;
+  english: string;
+};
