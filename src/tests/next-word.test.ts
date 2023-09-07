78a97fa184b6b5fcfc88c997a183b4ff2d4b18a7
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-07 16:23:11 -0700

    Move test variables to module scope

--- src/tests/next-word.test.ts
+++ src/tests/next-word.test.ts
@@ -2,6 +2,21 @@ import { describe, expect, it } from "vitest";
 import { nextWord } from "~/words/next-word";
 import { type Word } from "~/words/types";
 
+const timeNow = 100_000;
+const unseenWord: Word = { japanese: "1", english: "1" };
+const seenOnceWord15SecondsAgo: Word = {
+  japanese: "2",
+  english: "2",
+  lastSeen: timeNow - 15_000,
+  seenCount: 1,
+};
+const seenOnceWord10SecondsAgo: Word = {
+  japanese: "2",
+  english: "2",
+  lastSeen: timeNow - 10_000,
+  seenCount: 1,
+};
+
 describe("nextWord", () => {
   it("Returns undefined if there are no words", () => {
     expect(nextWord([], 0)).toEqual(undefined);
@@ -13,36 +28,17 @@ describe("nextWord", () => {
   });
 
   it("Returns the word with lowest lastSeen if that lastSeen was >=15 seconds ago", () => {
-    const word1: Word = { japanese: "1", english: "1" };
-    const word2: Word = {
-      japanese: "2",
-      english: "2",
-      lastSeen: 1,
-      seenCount: 1,
-    };
-    const word3: Word = {
-      japanese: "3",
-      english: "3",
-      lastSeen: 2,
-      seenCount: 1,
-    };
-    expect(nextWord([word1, word2, word3], 15_001)).toEqual(word2);
+    expect(
+      nextWord(
+        [unseenWord, seenOnceWord15SecondsAgo, seenOnceWord10SecondsAgo],
+        timeNow,
+      ),
+    ).toEqual(seenOnceWord15SecondsAgo);
   });
 
   it("Returns an unseen word if no words were last seen >=15 seconds ago", () => {
-    const word1: Word = { japanese: "1", english: "1" };
-    const word2: Word = {
-      japanese: "2",
-      english: "2",
-      lastSeen: 1,
-      seenCount: 1,
-    };
-    const word3: Word = {
-      japanese: "3",
-      english: "3",
-      lastSeen: 2,
-      seenCount: 1,
-    };
-    expect(nextWord([word1, word2, word3], 15_000)).toEqual(word1);
+    expect(nextWord([unseenWord, seenOnceWord10SecondsAgo], timeNow)).toEqual(
+      unseenWord,
+    );
   });
 });
