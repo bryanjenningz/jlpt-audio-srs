bc3d23338a4022d8570cf80fb4795aec6d3ab52d
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-07 10:50:14 -0700

    Remove boilerplate on home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
+import Head from "next/head";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>JLPT Audio SRS</title>
+        <meta
+          name="description"
+          content="Learn and review JLPT vocab by just listening"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
