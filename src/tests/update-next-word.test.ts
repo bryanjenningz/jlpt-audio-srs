1d0e12d03f87c0a5d31d22b14b2d7262a6b01380
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-07 17:53:39 -0700

    Add updateNextWord test for unseen, 10, 15, and 20 seconds ago

--- src/tests/update-next-word.test.ts
+++ src/tests/update-next-word.test.ts
@@ -77,4 +77,27 @@ describe("updateNextWord", () => {
       },
     ]);
   });
+
+  it("Updates the word 20 seconds ago if there's an unseen word, word seen once 10 seconds ago, a word seen once 15 seconds ago, and a word seen once 20 seconds ago", () => {
+    expect(
+      updateNextWord(
+        [
+          unseenWord,
+          wordSeenOnce10SecondsAgo,
+          wordSeenOnce15SecondsAgo,
+          wordSeenOnce20SecondsAgo,
+        ],
+        timeNow,
+      ),
+    ).toEqual([
+      unseenWord,
+      wordSeenOnce10SecondsAgo,
+      wordSeenOnce15SecondsAgo,
+      {
+        ...wordSeenOnce20SecondsAgo,
+        lastSeen: timeNow,
+        seenCount: 2,
+      },
+    ]);
+  });
 });
