cfecda36738ee5da1c9d2ac70c4d5b9af1de742a
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-08 14:45:22 -0700

    Separate tests into different describe blocks

--- src/tests/words/parse-words.test.ts
+++ src/tests/words/parse-words.test.ts
@@ -2,42 +2,46 @@ import { describe, expect, it } from "vitest";
 import { parseWords } from "~/words/parse-words";
 
 describe("parseWords", () => {
-  it("Throws if given an empty string", () => {
-    expect(() => parseWords("")).toThrowError(
-      new Error(`Expected 2 or 3 non-empty sections for line: ""`),
-    );
+  describe("Invalid strings", () => {
+    it("Throws if given an empty string", () => {
+      expect(() => parseWords("")).toThrowError(
+        new Error(`Expected 2 or 3 non-empty sections for line: ""`),
+      );
+    });
+
+    it("Throws if given a string with no semi-colons", () => {
+      expect(() => parseWords("word,pronunciation,definition")).toThrowError(
+        new Error(
+          `Expected 2 or 3 non-empty sections for line: "word,pronunciation,definition"`,
+        ),
+      );
+    });
+
+    it("Throws if there's a newline at the end of the string", () => {
+      expect(() =>
+        parseWords(
+          "word;pronunciation;definition\nword2;pronunciation2;definition2\n",
+        ),
+      ).toThrow(new Error(`Expected 2 or 3 non-empty sections for line: ""`));
+    });
   });
 
-  it("Throws if given a string with no semi-colons", () => {
-    expect(() => parseWords("word,pronunciation,definition")).toThrowError(
-      new Error(
-        `Expected 2 or 3 non-empty sections for line: "word,pronunciation,definition"`,
-      ),
-    );
-  });
+  describe("Lines with word, pronunciation, and definition", () => {
+    it("Parses a line with word, pronunciation, and definition separated by semi-colons", () => {
+      expect(parseWords("word;pronunciation;definition")).toEqual([
+        { type: "unseen", english: "definition", japanese: "word" },
+      ]);
+    });
 
-  it("Parses a line with word, pronunciation, and definition separated by semi-colons", () => {
-    expect(parseWords("word;pronunciation;definition")).toEqual([
-      { type: "unseen", english: "definition", japanese: "word" },
-    ]);
-  });
-
-  it("Parses 2 lines with word, pronunciation, and definition separated by semi-colons", () => {
-    expect(
-      parseWords(
-        "word;pronunciation;definition\nword2;pronunciation2;definition2",
-      ),
-    ).toEqual([
-      { type: "unseen", english: "definition", japanese: "word" },
-      { type: "unseen", english: "definition2", japanese: "word2" },
-    ]);
-  });
-
-  it("Throws if there's a newline at the end of the string", () => {
-    expect(() =>
-      parseWords(
-        "word;pronunciation;definition\nword2;pronunciation2;definition2\n",
-      ),
-    ).toThrow(new Error(`Expected 2 or 3 non-empty sections for line: ""`));
+    it("Parses 2 lines with word, pronunciation, and definition separated by semi-colons", () => {
+      expect(
+        parseWords(
+          "word;pronunciation;definition\nword2;pronunciation2;definition2",
+        ),
+      ).toEqual([
+        { type: "unseen", english: "definition", japanese: "word" },
+        { type: "unseen", english: "definition2", japanese: "word2" },
+      ]);
+    });
   });
 });
