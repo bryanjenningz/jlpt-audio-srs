481fd3088a66bf0b9b88768350c99afb45cdc5cf
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-08 14:32:06 -0700

    Move parseWords to new file

--- src/words/fetch.ts
+++ src/words/fetch.ts
@@ -1,21 +1,8 @@
 import { type Word } from "~/words/types";
+import { parseWords } from "~/words/parse-words";
 
 export async function fetchWords(): Promise<Word[]> {
   const response = await fetch("/jlpt5.txt");
   const text = await response.text();
   return parseWords(text);
-}
-
-export function parseWords(text: string): Word[] {
-  const lines = text.split("\n");
-  const words = lines.map((line): Word => {
-    const sections = line.split(";");
-    const japanese = sections[0];
-    const english = sections[sections.length - 1];
-    if (sections.length < 2 || sections.length > 3 || !japanese || !english) {
-      throw new Error(`Expected 2 or 3 non-empty sections for line: "${line}"`);
-    }
-    return { type: "unseen", japanese, english };
-  });
-  return words;
 }
