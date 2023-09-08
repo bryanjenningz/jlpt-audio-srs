c39585161e51f203bdb7e195eb18516e8fd6c3f4
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-07 17:38:37 -0700

    Use mock data in updateNextWord test

--- src/tests/update-next-word.test.ts
+++ src/tests/update-next-word.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { updateNextWord } from "~/words/update-next-word";
-
-const timeNow = 100_000;
+import { timeNow } from "~/tests/mock-data";
 
 describe("updateNextWord", () => {
   it("Returns empty array if you pass in an empty array", () => {
