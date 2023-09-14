f6308396ff57ac3bc8ae68b8210a70fe987a3564
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-14 16:06:52 -0700

    Validate all pitch accent lines have 3 sections

--- src/pitch-accents/validate.ts
+++ src/pitch-accents/validate.ts
@@ -1,7 +1,28 @@
 import fs from "fs/promises";
 
-const pitchAccentsText = (
+const pitchAccentLines = (
   await fs.readFile("./src/pitch-accents/pitch-accents.txt")
-).toString();
+)
+  .toString()
+  .split("\n")
+  .map((line) => line.split("\t"));
 
-console.log(pitchAccentsText.slice(0, 50));
+const errors = [
+  {
+    name: "Line doesn't have 3 sections",
+    lines: pitchAccentLines.filter((line) => line.length !== 3),
+  },
+].filter((category) => category.lines.length > 0);
+
+if (errors.length > 0) {
+  throw new Error(
+    errors
+      .map((error) => {
+        return [
+          error.name,
+          error.lines.map((line) => line.join("\t")).join("\n"),
+        ].join(":\n");
+      })
+      .join("\n\n"),
+  );
+}
