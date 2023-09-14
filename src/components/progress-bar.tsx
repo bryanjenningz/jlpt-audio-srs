e664c66dbb4af14150003b6ad40b9372404cfca4
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-14 15:33:02 -0700

    Only have 2 states for progress bar
    - Having 2 states is more simple and easy to understand
    - More information on a single state (percent and total)

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
 import { type Word } from "~/words/types";
 
-type ProgressBarKey = keyof typeof progressBarStates;
-
-const progressBarStates = {
-  0: "percent",
-  1: "complete",
-  2: "seenOrComplete",
-} as const;
+type ProgressBarState = "complete" | "seenOrComplete";
 
 export function ProgressBar({ words }: { words: Word[] }) {
-  const [percentCompleteShown, setPercentCompleteShown] =
-    useState<ProgressBarKey>(0);
+  const [state, setState] = useState<ProgressBarState>("complete");
   const completeCount = words.filter((word) => word.known).length;
   const completeOrSeenCount = words.filter(
     (word) => word.known || word.type === "seen",
@@ -26,47 +19,25 @@ export function ProgressBar({ words }: { words: Word[] }) {
       aria-hidden
       className="relative h-5 w-full overflow-hidden rounded-full bg-slate-800"
       onClick={() =>
-        setPercentCompleteShown((x): ProgressBarKey => {
-          switch (x) {
-            case 0:
-              return 1;
-            case 1:
-              return 2;
-            case 2:
-              return 0;
-          }
-        })
+        setState((x) => (x === "complete" ? "seenOrComplete" : "complete"))
       }
     >
       <div
         className="absolute bottom-0 left-0 top-0 bg-blue-700 text-center"
         style={{
-          width: ((): `${string}%` => {
-            switch (progressBarStates[percentCompleteShown]) {
-              case "percent":
-              case "complete":
-                return `${percentComplete}%`;
-              case "seenOrComplete":
-                return `${percentCompleteOrSeen}%`;
-            }
-          })(),
+          width:
+            state === "complete"
+              ? `${percentComplete}%`
+              : `${percentCompleteOrSeen}%`,
         }}
       ></div>
 
-      <div className="absolute inset-0 flex items-center justify-center">
-        {((): string => {
-          if (words.length === 0) {
-            return "";
-          }
-          switch (progressBarStates[percentCompleteShown]) {
-            case "percent":
-              return `${percentComplete}% complete`;
-            case "complete":
-              return `${completeCount} / ${words.length} complete`;
-            case "seenOrComplete":
-              return `${completeOrSeenCount} / ${words.length} seen or complete`;
-          }
-        })()}
+      <div className="absolute inset-0 flex items-center justify-center text-xs">
+        {words.length === 0
+          ? ""
+          : state === "complete"
+          ? `${completeCount} / ${words.length} (${percentComplete}%) complete`
+          : `${completeOrSeenCount} / ${words.length} (${percentCompleteOrSeen}%) seen or complete`}
       </div>
     </button>
   );
