c08f43d07120aade7ef6b42fd1cfa441e7d15adc
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-16 15:09:00 -0700

    Validate no duplicate kanji in JLPT text files

--- src/scripts/validate-jlpt-text.ts
+++ src/scripts/validate-jlpt-text.ts
@@ -15,6 +15,21 @@ for (const filePath of FILE_PATHS) {
       name: `Lines must have 2 or 3 sections separated by "${FIELD_SEPARATOR}"`,
       lines: lines.filter((line) => !(line.length === 2 || line.length === 3)),
     },
+    {
+      name: `All kanji must be unique`,
+      lines: (() => {
+        const kanjiCounts = count(lines.map((line) => line[0]));
+        return lines.filter((line) => (kanjiCounts.get(line[0]) ?? 0) > 1);
+
+        function count<T>(values: Iterable<T>): Map<T, number> {
+          const counts = new Map<T, number>();
+          for (const value of values) {
+            counts.set(value, (counts.get(value) ?? 0) + 1);
+          }
+          return counts;
+        }
+      })(),
+    },
   ].filter((category) => category.lines.length > 0);
 
   if (errors.length > 0) {
