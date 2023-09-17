6eae991f11cb058a0e8c680447f5494d62650797
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-16 18:29:40 -0700

    Remove duplicate definitions in JLPT text files

--- src/scripts/validate-jlpt-text.ts
+++ src/scripts/validate-jlpt-text.ts
@@ -5,21 +5,70 @@ const ENTRY_SEPARATOR = "\n";
 const FIELD_SEPARATOR = ";";
 
 for (const filePath of FILE_PATHS) {
-  const lines = (await fs.readFile(filePath))
+  const entries = (await fs.readFile(filePath))
     .toString()
     .split(ENTRY_SEPARATOR)
-    .map((line) => line.split(FIELD_SEPARATOR));
+    .map((line) => {
+      const sections = line.split(FIELD_SEPARATOR);
+      if (
+        sections.length < 2 ||
+        sections.length > 3 ||
+        !sections[0] ||
+        !sections[1]
+      ) {
+        throw new Error(`Each line must have 2 or 3 sections: "${line}"`);
+      }
+      if (sections[2]) {
+        return {
+          kanji: sections[0].trim(),
+          kana: sections[1].trim(),
+          definition: sections[2].trim(),
+        };
+      }
+      return {
+        kanji: sections[0].trim(),
+        kana: sections[0].trim(),
+        definition: sections[1].trim(),
+      };
+    });
 
   const errors = [
     {
       name: `Lines must have 2 or 3 sections separated by "${FIELD_SEPARATOR}"`,
-      lines: lines.filter((line) => !(line.length === 2 || line.length === 3)),
+      lines: entries.filter(
+        (entry) => !entry.kanji || !entry.kana || !entry.definition,
+      ),
     },
     {
       name: `All kanji must be unique`,
       lines: (() => {
-        const kanjiCounts = count(lines.map((line) => line[0]));
-        return lines.filter((line) => (kanjiCounts.get(line[0]) ?? 0) > 1);
+        const kanjiCounts = count(entries.map((entry) => entry.kanji));
+        return entries.filter(
+          (entry) => (kanjiCounts.get(entry.kanji) ?? 0) > 1,
+        );
+
+        function count<T>(values: Iterable<T>): Map<T, number> {
+          const counts = new Map<T, number>();
+          for (const value of values) {
+            counts.set(value, (counts.get(value) ?? 0) + 1);
+          }
+          return counts;
+        }
+      })(),
+    },
+    {
+      name: `All definitions must be unique`,
+      lines: (() => {
+        const definitionCounts = count(
+          entries.map((entry) => entry.definition),
+        );
+        return entries
+          .filter((entry) => (definitionCounts.get(entry.definition) ?? 0) > 1)
+          .sort((a, b) => {
+            if (a.definition < b.definition) return -1;
+            if (a.definition > b.definition) return 1;
+            return 0;
+          });
 
         function count<T>(values: Iterable<T>): Map<T, number> {
           const counts = new Map<T, number>();
@@ -40,7 +89,11 @@ for (const filePath of FILE_PATHS) {
             return [
               error.name,
               error.lines
-                .map((line) => line.join(FIELD_SEPARATOR))
+                .map((entry) =>
+                  [entry.kanji, entry.kana, entry.definition].join(
+                    FIELD_SEPARATOR,
+                  ),
+                )
                 .join(ENTRY_SEPARATOR),
             ].join(":\n");
           })
