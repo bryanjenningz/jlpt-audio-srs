a81e7904d7a60807addae5e0cf41d6052ecbc860
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-22 15:37:10 -0700

    Make service worker type-safe
    - See https://joshuatz.com/posts/2021/strongly-typed-service-workers/

--- public/service-worker.mjs
+++ public/service-worker.mjs
@@ -1,9 +1,22 @@
-console.log("service worker running");
+// @ts-check
+/// <reference no-default-lib="false"/>
+/// <reference lib="ES2015" />
+/// <reference lib="webworker" />
 
-self.addEventListener("install", () => {
-  console.log("service worker installing");
-});
+// Using IIFE to provide closure to redefine `self`
+(() => {
+  // This is a little messy, but necessary to force type assertion
+  // Same issue as in TS -> https://github.com/microsoft/TypeScript/issues/14877
+  // prettier-ignore
+  const self = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (globalThis.self));
 
-self.addEventListener("activate", () => {
-  console.log("service worker activating");
-});
+  console.log("Running service worker");
+
+  self.addEventListener("install", () => {
+    console.log("Installing service worker");
+  });
+
+  self.addEventListener("activate", () => {
+    console.log("Activating service worker");
+  });
+})();
