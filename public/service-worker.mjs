08bea4e2e6d648dba95d8dcce406509b4dd967c8
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-22 16:56:51 -0700

    Remove unnecessary immediately invoked function expression (IIFE)

--- public/service-worker.mjs
+++ public/service-worker.mjs
@@ -1,59 +1,55 @@
 /// <reference lib="webworker" />
 
-// Using IIFE to provide closure to redefine `self`
-(() => {
-  // This is a little messy, but necessary to force type assertion
-  // Same issue as in TS -> https://github.com/microsoft/TypeScript/issues/14877
-  // prettier-ignore
-  const self = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (globalThis.self));
+const self = /** @type {ServiceWorkerGlobalScope} */ (
+  /** @type {unknown} */ (globalThis.self)
+);
 
-  const CACHE_NAME = "jlpt-audio-srs-pages";
-  const CACHED_FILES = [
-    "/favicon.ico",
-    "/jlpt1.txt",
-    "/jlpt2.txt",
-    "/jlpt3.txt",
-    "/jlpt4.txt",
-    "/jlpt5.txt",
+const CACHE_NAME = "jlpt-audio-srs-pages";
+const CACHED_FILES = [
+  "/favicon.ico",
+  "/jlpt1.txt",
+  "/jlpt2.txt",
+  "/jlpt3.txt",
+  "/jlpt4.txt",
+  "/jlpt5.txt",
 
-    "/",
+  "/",
 
-    "/learn/5",
-    "/learn/4",
-    "/learn/3",
-    "/learn/2",
-    "/learn/1",
+  "/learn/5",
+  "/learn/4",
+  "/learn/3",
+  "/learn/2",
+  "/learn/1",
 
-    "/words/5",
-    "/words/4",
-    "/words/3",
-    "/words/2",
-    "/words/1",
+  "/words/5",
+  "/words/4",
+  "/words/3",
+  "/words/2",
+  "/words/1",
 
-    "/settings",
-  ];
+  "/settings",
+];
 
-  self.addEventListener("install", (event) => {
-    event.waitUntil(
-      (async () => {
-        const cache = await caches.open(CACHE_NAME);
-        await cache.addAll(CACHED_FILES);
-      })(),
-    );
-  });
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      await cache.addAll(CACHED_FILES);
+    })(),
+  );
+});
 
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      (async () => {
-        const cache = await caches.open(CACHE_NAME);
-        const response = await cache.match(event.request.url, {
-          ignoreSearch: true,
-        });
-        if (!response) {
-          console.log("Uncached response for request", event.request);
-        }
-        return response ?? fetch(event.request);
-      })(),
-    );
-  });
-})();
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const response = await cache.match(event.request.url, {
+        ignoreSearch: true,
+      });
+      if (!response) {
+        console.log("Uncached response for request", event.request);
+      }
+      return response ?? fetch(event.request);
+    })(),
+  );
+});
