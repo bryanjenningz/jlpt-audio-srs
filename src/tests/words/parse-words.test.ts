ea4c66b10d092dbfa5b2cba517f327e962b00120
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-22 18:27:00 -0700

    Update parse word tests

--- src/tests/words/parse-words.test.ts
+++ src/tests/words/parse-words.test.ts
@@ -6,14 +6,14 @@ describe("parseWords", () => {
   describe("Invalid strings", () => {
     it("Throws if given an empty string", () => {
       expect(() => parseWords("")).toThrowError(
-        new Error(`Expected 2 or 3 non-empty sections for line: ""`),
+        new Error(`Expected 4 sections for line: ""`),
       );
     });
 
     it("Throws if given a string with no semi-colons", () => {
-      expect(() => parseWords("word,pronunciation,definition")).toThrowError(
+      expect(() => parseWords("word,pronunciation,definition,")).toThrowError(
         new Error(
-          `Expected 2 or 3 non-empty sections for line: "word,pronunciation,definition"`,
+          `Expected 4 sections for line: "word,pronunciation,definition,"`,
         ),
       );
     });
@@ -21,13 +21,13 @@ describe("parseWords", () => {
     it("Throws if there's a newline at the end of the string", () => {
       expect(() =>
         parseWords(
-          "word;pronunciation;definition\nword2;pronunciation2;definition2\n",
+          "word;pronunciation;definition;\nword2;pronunciation2;definition2;\n",
         ),
-      ).toThrow(new Error(`Expected 2 or 3 non-empty sections for line: ""`));
+      ).toThrow(new Error(`Expected 4 sections for line: ""`));
     });
   });
 
-  describe("Lines with word, pronunciation, and definition", () => {
+  describe("Lines with word, pronunciation, definition, and empty pitch accents", () => {
     it("Parses a line with word, pronunciation, and definition separated by semi-colons", () => {
       const result: Word[] = [
         {
@@ -40,7 +40,7 @@ describe("parseWords", () => {
           known: false,
         },
       ];
-      expect(parseWords("word;pronunciation;definition")).toEqual(result);
+      expect(parseWords("word;pronunciation;definition;")).toEqual(result);
     });
 
     it("Parses 2 lines with word, pronunciation, and definition separated by semi-colons", () => {
@@ -66,7 +66,7 @@ describe("parseWords", () => {
       ];
       expect(
         parseWords(
-          "word;pronunciation;definition\nword2;pronunciation2;definition2",
+          "word;pronunciation;definition;\nword2;pronunciation2;definition2;",
         ),
       ).toEqual(result);
     });
@@ -94,50 +94,9 @@ describe("parseWords", () => {
       ];
       expect(
         parseWords(
-          "word;pronunciation;definition,definition-b\nword2;pronunciation2;definition2,definition-b,definition-c",
+          "word;pronunciation;definition,definition-b;\nword2;pronunciation2;definition2,definition-b,definition-c;",
         ),
       ).toEqual(result);
-    });
-  });
-
-  describe("Lines with word and definition", () => {
-    it("Parses a line with word and definition separated by a semi-colon", () => {
-      const result: Word[] = [
-        {
-          type: "unseen",
-          definition: "definition",
-          kanji: "word",
-          kana: "word",
-          pitchAccents: [],
-          order: 1,
-          known: false,
-        },
-      ];
-      expect(parseWords("word;definition")).toEqual(result);
-    });
-
-    it("Parses 2 lines with word and definition separated by a semi-colon", () => {
-      const result: Word[] = [
-        {
-          type: "unseen",
-          definition: "definition",
-          kanji: "word",
-          kana: "word",
-          pitchAccents: [],
-          order: 1,
-          known: false,
-        },
-        {
-          type: "unseen",
-          definition: "definition2",
-          kanji: "word2",
-          kana: "word2",
-          pitchAccents: [],
-          order: 2,
-          known: false,
-        },
-      ];
-      expect(parseWords("word;definition\nword2;definition2")).toEqual(result);
     });
   });
 });
