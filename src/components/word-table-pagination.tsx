10f58fa66c701abe56abe5e887d07249e6078df3
Bryan Jennings <bryanjenningz@gmail.com>
2023-09-24 17:30:34 -0700

    Add keyboard arrow icons to word table pagination buttons

--- src/components/word-table-pagination.tsx
+++ src/components/word-table-pagination.tsx
@@ -1,3 +1,5 @@
+import { KeyboardArrowLeftIcon } from "~/icons/keyboard-arrow-left-icon";
+import { KeyboardArrowRightIcon } from "~/icons/keyboard-arrow-right-icon";
 import { useStore } from "~/stores/use-store";
 import {
   DEFAULT_PAGE,
@@ -17,25 +19,33 @@ export function WordTablePagination() {
   const decrementPage = useWordTablePaginationStore((x) => x.decrementPage);
   const incrementPage = useWordTablePaginationStore((x) => x.incrementPage);
 
+  console.log({ wordsLength });
+
+  if (wordsLength === 0) {
+    <></>;
+  }
+
   return (
     <div className="flex w-full items-center justify-between">
       <button
-        className="h-10 w-10 rounded-full bg-slate-700"
+        className="flex h-10 w-10 items-center justify-center rounded-full bg-slate-700"
         disabled={page <= 0}
         onClick={() => decrementPage(level)}
+        title="Previous page"
       >
-        {`<`}
+        <KeyboardArrowLeftIcon />
       </button>
       {`${page * WORDS_PER_PAGE + 1} - ${Math.min(
         wordsLength,
         (page + 1) * WORDS_PER_PAGE,
       )}`}
       <button
-        className="h-10 w-10 rounded-full bg-slate-700"
+        className="flex h-10 w-10 items-center justify-center rounded-full bg-slate-700"
         disabled={(page + 1) * WORDS_PER_PAGE >= wordsLength}
         onClick={() => incrementPage(level)}
+        title="Next page"
       >
-        {`>`}
+        <KeyboardArrowRightIcon />
       </button>
     </div>
   );
